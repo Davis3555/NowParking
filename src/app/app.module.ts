0ea6e5a0830f2118e1ce0525afb51b648032a596
Davis3555 <101678446+Davis3555@users.noreply.github.com>
2022-11-02 23:13:48 -0300

    agregue geolocalzacion por consola

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { Geolocation } from '@awesome-cordova-plugins/geolocation/ngx';//importacion de libreria para el uso de geolocalizacion
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [Geolocation, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],//se implementa la variable Geolocation
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/mapa/mapa.page.ts
+++ src/app/mapa/mapa.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Geolocation, Geoposition } from '@awesome-cordova-plugins/geolocation/ngx';//importacion de libreria para el uso de geolocalizacion
+
 @Component({
   selector: 'app-mapa',
   templateUrl: './mapa.page.html',
@@ -7,8 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapaPage implements OnInit {
 
-  constructor() { }
+  constructor(public geolocation: Geolocation) { //se determina el constructor con variable public
+  
+  }
+  
+  ngAfterViewInit() {
 
+    this.geolocationNative()
+  }
+  
+  geolocationNative(){
+    
+    this.geolocation.getCurrentPosition().then((geposition: Geoposition) => {
+
+      console.log(geposition);
+    })
+  }
+  
+  
   ngOnInit() {
   }
 
